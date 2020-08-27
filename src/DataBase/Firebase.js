7fed0a401f3a79123e30d5118dcb69fe6255a1c1
Erik Engholm <45353988+Erikengholm@users.noreply.github.com>
2020-08-27 14:31:05 +0200

    Add files via upload

---
+++ src/DataBase/Firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+let config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+    apiKey: "AIzaSyAyooVNL_4keVvsAHK2zGuod03UksKjLzQ",
+    authDomain: "car-database-c2c5c.firebaseapp.com",
+    databaseURL: "https://car-database-c2c5c.firebaseio.com",
+    projectId: "car-database-c2c5c",
+    storageBucket: "car-database-c2c5c.appspot.com",
+    messagingSenderId: "605463403199",
+    appId: "1:605463403199:web:3fde78eaab9d08f71d840a",
+    measurementId: "G-CG046FQQH3"
+};
+firebase.initializeApp(config);
+
+export default firebase;
---
+++ src/components/car_holder.js
@@ -0,0 +1,18 @@
+import React,{ useState } from "react";
+import Information from "../DataBase/Database"
+const CARS = () =>{
+    return(
+        <div className="card">
+            <h5>{Information.head}</h5>
+            <img/>
+            <p></p>
+            <span>
+            <div></div>
+                <a></a>
+            <div></div>
+            </span>
+        </div>);
+
+}
+
+export default CARS
